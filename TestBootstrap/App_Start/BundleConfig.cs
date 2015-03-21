b634e5a3448ba970dd66f925104bfcb7f23381f4
Matthew Krizanac <matthewkrizanac@gmail.com>
2015-03-21 13:57:47 -0500

    Added test site for compiling less

---
+++ TestBootstrap/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Optimization;
+
+namespace TestBootstrap
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/scripts/jquery").Include(
+                        "~/Scripts/jquery-{version}.min.js",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/scripts/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.min.js"
+                      ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/Site.min.css"));
+        }
+    }
+}
---
+++ TestBootstrap/Controllers/NavigationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestBootstrap.Controllers
+{
+    public class NavController : Controller
+    {
+        #region Main
+
+        public ActionResult Index()
+        {
+            return View("../Main/Index");
+        }
+
+        #endregion
+
+    }
+}
